05dee7faa52ab6573facda506aa078a9686c27bb
kota0 <kota.mac07@gmail.com>
2022-04-17 21:20:13 +0900

    マイグレーションファイルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,25 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #  Include default devise modules. Others available are:
+  #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email,presence: true, uniqueness: ture, inclusion: { in: '@' }
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :encrypted_password, presence: true, :content, {length: {minimum:6}}, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'},
-            confirmation: true
-  validates :nickname
+  
+   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+   validates :encrypted_password, format:{ with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
+   validates :last_name_kana, format:{ with: /\A[\p{katakana}\p{blank}ーー]+\z/, message: 'はカタカナで入力して下さい。'}, presence: true
+   validates :first_name_kana, format:{ with: /\A[\p{katakana}\p{blank}ーー]+\z/, message: 'はカタカナで入力して下さい。'}, presence: true
+   validates :birthday, presence: true
+
+
+   with_options presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+   validates :first_name
+   validates :last_name
   end
   
-  validates :last_name_kana, format:presence: true, { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'    
-  validates :first_name_kana, format:presence: true, { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'
-  validates :birthday,presence: true
 
-  has_many :items
-  has_many :buys
+   has_many :items
+   has_many :buys
 
-end
+ end
