7fc152f139d6d17d23084a66747ae651c75953e9
kota0 <kota.mac07@gmail.com>
2022-04-19 11:43:59 +0900

    ユーザー管理実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  #  Include default devise modules. Others available are:
-  #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   
    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-   validates :encrypted_password, format:{ with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+   validates :password, format:{ with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
    validates :last_name_kana, format:{ with: /\A[\p{katakana}\p{blank}ーー]+\z/, message: 'はカタカナで入力して下さい。'}, presence: true
    validates :first_name_kana, format:{ with: /\A[\p{katakana}\p{blank}ーー]+\z/, message: 'はカタカナで入力して下さい。'}, presence: true
