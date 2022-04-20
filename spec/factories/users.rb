b72c91b4c76e44de82f76fb6205e434206e00107
kota0 <kota.mac07@gmail.com>
2022-04-20 21:47:25 +0900

    ユーザー管理（テスト修正）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname                {'test'}
     email                   {Faker::Internet.free_email}
-    password                {Faker::Internet.password(min_length: 6)}
+    # password                {Faker::Internet.password(min_length: 6, min_alpha: 1, min_numeric: 1)}
+    password                {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation   {password}
     first_name              {'全角'}
     last_name               {'全角'}
